d6efa0f93d5c453be467f0618d3cca8a4083feca
Dacn0210 <dacn2004@gmail.com>
2024-06-02 15:16:17 -0500

    Prueba base de Datos

--- model/conexion.php
+++ model/conexion.php
@@ -1,5 +1,8 @@
 <?php
-    $conexion = new mysqli("localhost","u440059004_root","eleDHWJD_0610.","u440059004_electrotech"); //conexion a db
+    $conexion = new mysqli("localhost","u440059004_pedro","@qiiraK0d","u440059004_pedro"); //conexion a db
     $conexion ->set_charset("utf8"); //admitir caracteres especiales
+
+    // $conexion = new mysqli("localhost","u440059004_root","eleDHWJD_0610.","u440059004_electrotech"); //conexion a db
+    // $conexion ->set_charset("utf8"); //admitir caracteres especiales
 
 //Conexion lista :3
