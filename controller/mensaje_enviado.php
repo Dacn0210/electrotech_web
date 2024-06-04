ab4eb6cee77fe359800403cb351888a37ab845b8
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2024-06-04 15:19:45 -0500

    Cambio ene l mensaje de envio de contraseña

--- controller/mensaje_enviado.php
+++ controller/mensaje_enviado.php
@@ -16,11 +16,11 @@
         <div class="message-container">
             <img src="../public/img/Logo.png" alt="Logo de la empresa">
             <span id="check"><i class="fa-regular fa-circle-check"></i></span>
-            <p>Se ha enviado un correo electrónico con el código de recuperación</p>
+            <p>Se ha enviado un correo electrónico con el código de recuperación, verifica tu bandeja de entrada</p>
         </div>
     </div>
     <a href="../index.php"><i class="fa-solid fa-house"></i> Volver al inicio </a>
-    
+
     <script src="https://kit.fontawesome.com/909a90592e.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
