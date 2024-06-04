597a28036cc64e857b9bedaf533060cafc5c9d56
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2024-06-04 15:15:55 -0500

    Cambio de posicion de boton volver al inicio

--- controller/mensaje_enviado.php
+++ controller/mensaje_enviado.php
@@ -18,9 +18,9 @@
             <span id="check"><i class="fa-regular fa-circle-check"></i></span>
             <p>Se ha enviado un correo electrónico con el código de recuperación</p>
         </div>
-        <a href="../index.php"><i class="fa-solid fa-house"></i> Volver al inicio </a>
     </div>
-
+    <a href="../index.php"><i class="fa-solid fa-house"></i> Volver al inicio </a>
+    
     <script src="https://kit.fontawesome.com/909a90592e.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
