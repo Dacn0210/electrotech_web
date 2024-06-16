1e9966538066d1707b8003a5dd100e7c3adb3002
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2024-06-15 19:08:20 -0500

    Prueba 2 editar perfil

--- controller/mostrardatosperfil.php
+++ controller/mostrardatosperfil.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "../../../model/conexion.php";
 
 if (!isset($_SESSION['usuario'])) {
     header("Location: ../view/login.php");
@@ -8,18 +9,14 @@ if (!isset($_SESSION['usuario'])) {
 
 $usuario = $_SESSION['usuario'];
 
-include 'conexion.php';
-
 $consulta = "SELECT * FROM usuarios WHERE username='$usuario'";
 $resultado = $conexion->query($consulta);
 
 if ($resultado && $resultado->num_rows > 0) {
     $usuarioData = $resultado->fetch_assoc();
-    $_SESSION['usuarioData'] = $usuarioData;
 } else {
     echo "<script>alert('No se encontraron los datos del usuario');</script>";
 }
 
 $resultado->free_result();
 $conexion->close();
-?>
